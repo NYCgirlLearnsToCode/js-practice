5a89946d2aed561fe120cf844fc230855a5dbd41
LJ <imnotlostrui@aim.com>
2017-07-02 23:26:59 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var myString = "Lisa";
+var myNumber = 7;
+var myBoolean = true;
+
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
