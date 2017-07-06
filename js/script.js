e71d3a8eb0059b406825f5b8eb70972de6fd0932
LJ <imnotlostrui@aim.com>
2017-07-06 13:34:12 -0400

    practiced var, functions, if/else, switch,return

--- js/script.js
+++ js/script.js
@@ -5,3 +5,63 @@ var myBoolean = true;
 console.log("Name: " + myString);
 console.log("Lucky Number: " + myNumber);
 console.log("Good joke? " + myBoolean);
+//var practice ----------------------------------
+
+var strength = '50,000 pounds';
+var age = 26;
+var hasPet = true;
+
+console.log(hasPet);
+console.log(age);
+console.log('How much stuff can a variable hold? ', strength);
+
+// if statement practice-----------------------------
+var hungerLevel = 10;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log('Let\'s eat later');
+}
+
+// switch statement practice----------------
+var moonPhase = 'mostly new';
+switch (moonPhase) {
+  case 'full':
+   console.log('Howwwwlll!!');
+    break;
+  case 'mostly full': 
+  console.log('Arms and legs are getting hairier');
+  	break;
+  case 'mostly new': 
+  console.log('Back on two feet');
+		break;
+  default:
+  console.log('Invalid moon phase');
+    break;
+}
+
+// functions with returns------------
+var orderCount=0;
+function takeOrder(topping, crustType) {
+  orderCount = orderCount + 1;
+  console.log('Order: '+ crustType + ' pizza topped with ' + topping);
+}
+
+function getSubTotal(itemCount){
+	return itemCount*7.5;  
+}
+
+function getTax(){
+  return getSubTotal(orderCount)*0.06;
+}
+
+function getTotal(){
+  return getSubTotal(orderCount)+getTax();
+}
+
+takeOrder('thin','mushroom');
+takeOrder('bacon','cheesy');
+takeOrder('peppers', 'thick');
+console.log(getSubTotal(orderCount));
+console.log(getTotal());
